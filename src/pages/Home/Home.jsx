1cf3fb01785f89d300af1cb2c401f7377dc9ce6e
saikumar080 <pillalamarrisaikumar8@gmail.com>
2026-07-12 21:11:17 +0530

    Fixed the issues in navbar

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from '../../components/Navbar/NavBar'
+import Navbar from '../../components/Navbar/Navbar'
 import HeroSection from '../../components/HeroSection/HeroSection';
 function HomePage() {
   return (
